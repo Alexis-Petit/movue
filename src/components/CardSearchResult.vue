2752ee08d9c4ae6db537dc6d89620ab32a46bdd2
Alexis Petit <alexispetit1@protonmail.com>
2021-03-24 16:49:09 +0100

    setup search bar and results

---
+++ src/components/CardSearchResult.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img :src="imgSrc" alt="Placeholder image" />
+      </figure>
+    </div>
+    <div class="card-content ">
+      <div class="media ">
+        <div class="media-content">
+          <p class="title is-4">{{ cardResult.title }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CardResult",
+  props: ["cardResult"],
+  data() {
+    return {
+      imgSrc: this.cardResult.image,
+    };
+  },
+  mounted() {
+    // this.imgSrc = String(this.cardFilm.image).slice(0, this.cardFilm.image.length - 3);
+    this.imgSrc = String(this.cardResult.image).slice(
+      0,
+      this.cardResult.image.length - 4
+    );
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
