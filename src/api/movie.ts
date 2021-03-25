337a06d70ffea1f11ef1b3582976cab8e91bc61b
Alexis Petit <alexispetit1@protonmail.com>
2021-03-25 19:36:03 +0100

    Top view isok

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -5,6 +5,10 @@ const VUE_APP_IN_THEATERS = process.env.VUE_APP_IN_THEATERS;
 const VUE_APP_COMING_SOON = process.env.VUE_APP_COMING_SOON;
 const VUE_APP_YOUTUBETRAILER = process.env.VUE_APP_YOUTUBETRAILER;
 const VUE_APP_FILMID = process.env.VUE_APP_FILMID;
+const VUE_APP_FILMSEARCH = process.env.VUE_APP_FILMSEARCH;
+const VUE_APP_TOP250 = process.env.VUE_APP_TOP250;
+const VUE_APP_MOST_POPULAR_MOVIES = process.env.VUE_APP_MOST_POPULAR_MOVIES;
+const VUE_APP_MOST_POPULAR_SERIES = process.env.VUE_APP_MOST_POPULAR_SERIES;
 
 export const movieInTheaters = async () => {
   let results;
@@ -73,6 +77,90 @@ export const movieWithId = async (idVideo: string) => {
     const response = await axios.get(url);
     console.log(response);
     results = response.data;
+  } catch (err) {
+    if (err.response) {
+      // client received an error response (5xx, 4xx)
+      console.log("Server Error:", err);
+    } else if (err.request) {
+      // client never received a response, or request never left
+      console.log("Network Error:", err);
+    } else {
+      console.log("Client Error:", err);
+    }
+  }
+  return results;
+};
+
+export const search = async (query: string) => {
+  let results;
+  try {
+    const url = `${VUE_APP_FILMSEARCH}${API_KEY}/${query}`;
+    const response = await axios.get(url);
+    console.log(response);
+    results = response.data.results;
+  } catch (err) {
+    if (err.response) {
+      // client received an error response (5xx, 4xx)
+      console.log("Server Error:", err);
+    } else if (err.request) {
+      // client never received a response, or request never left
+      console.log("Network Error:", err);
+    } else {
+      console.log("Client Error:", err);
+    }
+  }
+  return results;
+};
+
+export const top250 = async () => {
+  let results;
+  try {
+    const url = `${VUE_APP_TOP250}${API_KEY}`;
+    const response = await axios.get(url);
+    results = response.data.items.slice(0,10);
+    console.log(results);
+  } catch (err) {
+    if (err.response) {
+      // client received an error response (5xx, 4xx)
+      console.log("Server Error:", err);
+    } else if (err.request) {
+      // client never received a response, or request never left
+      console.log("Network Error:", err);
+    } else {
+      console.log("Client Error:", err);
+    }
+  }
+  return results;
+};
+
+export const mostPopularMovies = async () => {
+  let results;
+  try {
+    const url = `${VUE_APP_MOST_POPULAR_MOVIES}${API_KEY}`;
+    const response = await axios.get(url);
+    results = response.data.items.slice(0,10);
+    console.log(results) 
+  } catch (err) {
+    if (err.response) {
+      // client received an error response (5xx, 4xx)
+      console.log("Server Error:", err);
+    } else if (err.request) {
+      // client never received a response, or request never left
+      console.log("Network Error:", err);
+    } else {
+      console.log("Client Error:", err);
+    }
+  }
+  return results;
+};
+
+export const mostPopularSeries = async () => {
+  let results;
+  try {
+    const url = `${VUE_APP_MOST_POPULAR_SERIES}${API_KEY}`;
+    const response = await axios.get(url);
+    results = response.data.items.slice(0,10);
+    console.log(results) 
   } catch (err) {
     if (err.response) {
       // client received an error response (5xx, 4xx)
--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,15 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="container is-fluid">
+    <section class="section">
+      <div class="content level">
+        <h1 class="level-left">Trending movies :</h1>
+      </div>
+      <div class="columns is-multiline is-centered">
+        <CardFilm
+          v-for="cardFilm in trendingMovies"
+          :key="cardFilm.id"
+          class="column is-half-tablet is-one-fifth-desktop is-one-fifth-widescreen m-3"
+          :cardFilm="cardFilm"
+          @click="showDetail(cardFilm)"
+        />
+      </div>
+    </section>
+    <section class="section">
+      <div class="content level">
+        <h1 class="level-left">Trending TV shows :</h1>
+      </div>
+      <div class="columns is-multiline is-centered">
+        <CardFilm
+          v-for="cardFilm in trendingSeries"
+          :key="cardFilm.id"
+          class="column is-half-tablet is-one-fifth-desktop is-one-fifth-widescreen m-3"
+          :cardFilm="cardFilm"
+          @click="showDetail(cardFilm)"
+        />
+      </div>
+    </section>
+    <section class="section">
+      <div class="content level">
+        <h1 class="level-left">Top movies :</h1>
+      </div>
+      <div class="columns is-multiline is-centered">
+        <CardFilm
+          v-for="cardFilm in topMovies"
+          :key="cardFilm.id"
+          class="column is-half-tablet is-one-fifth-desktop is-one-fifth-widescreen m-3"
+          :cardFilm="cardFilm"
+          @click="showDetail(cardFilm)"
+        />
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import CardFilm from "@/components/CardFilm.vue"; // @ is an alias to /src
+import { top250, mostPopularMovies, mostPopularSeries } from "../api/movie";
 
 export default defineComponent({
   name: "Top",
   components: {
-  }
+    CardFilm,
+  },
+  data() {
+    return {
+      topMovies: [],
+      trendingMovies: [],
+      trendingSeries: []
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      this.topMovies = await top250()
+      this.trendingMovies = await mostPopularMovies();
+      this.trendingSeries = await mostPopularSeries();
+    },
+    showDetail(cardFilm: { id: any; }) {
+      this.$router.push({
+        name: "CardFilmDetails",
+        params: { id: cardFilm.id },
+      });
+    },
+  },
 });
 </script>
